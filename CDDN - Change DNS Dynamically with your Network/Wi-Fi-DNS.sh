693034c2f00df85c44fb00c23caee80c87aac661
Andrea Bacciu <36055796+andreabac3@users.noreply.github.com>
2019-02-21 11:20:02 +0100

    Add files via upload

---
+++ CDDN - Change DNS Dynamically with your Network/Wi-Fi-DNS.sh
@@ -0,0 +1,34 @@
+#!/bin/bash          
+dnsConfig="dnsConfig.txt"
+
+
+while getopts l option
+    do
+    case "${option}"
+    in
+    l) echo $(cat $dnsConfig) && exit 0;;
+    esac
+done
+
+if [[ ! -e $dnsConfig ]]; then
+    touch $dnsConfig  && echo "replace_with_name_of_your_Wifi | 1.1.1.1 1.0.0.1 ;" >> $dnsConfig 
+    echo "Modify dnsConfig.txt with your custom settings & restart the .sh"
+    exit 0
+fi
+
+PATTERN="replace_with_name_of_your_Wifi"
+if [ $(cat $dnsConfig  | wc -l) -eq "1" ]; then
+   if grep -q $PATTERN $dnsConfig ; then
+        echo "Modify $dnsConfig  with your custom settings & restart the .sh"
+        exit 1
+    fi 
+fi
+
+
+os=$(uname)
+if [[ $os -eq "Darwin" ]]; then 
+    wifiName=$(/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -I | awk '/ SSID/ {print substr($0, index($0, $2))}')
+    card=$(networksetup -listallnetworkservices | grep Wi-Fi)
+    dns=$(cat $dnsConfig | grep "$wifiName" | sed -e 's/\(^.*|\)\(.*\)\(;.*$\)/\2/')
+    $(networksetup -setdnsservers Wi-Fi $dns) && echo "I'have correctly set the DNS $dns for the SSID network called $wifiName"
+fi
