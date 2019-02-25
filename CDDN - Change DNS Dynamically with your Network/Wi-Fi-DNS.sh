6c4e9d99b7c008f058b53b1117fd61e8c6a9991d
Andrea Bacciu <36055796+andreabac3@users.noreply.github.com>
2019-02-25 10:48:42 +0100

    Update Wi-Fi-DNS.sh

--- CDDN - Change DNS Dynamically with your Network/Wi-Fi-DNS.sh
+++ CDDN - Change DNS Dynamically with your Network/Wi-Fi-DNS.sh
@@ -24,8 +24,9 @@ if [[ ! -e "$dnsConfig" ]]; then
     exit 0
 fi
 
-PATTERN="replace_with_name_of_your_Wifi"
+
 if [ $(wc -l "$dnsConfig" | grep -o "[0-9]\+" ) -eq "0" ]; then
+   PATTERN="replace_with_name_of_your_Wifi"
    if [ ! $(cat "$dnsConfig" | grep -q "$PATTERN") ]; then
         echo "Modify $dnsConfig  with your custom settings & restart the .sh"
         exit 1
@@ -38,12 +39,12 @@ function macOS {
     card=$(networksetup -listallnetworkservices | grep Wi-Fi)
     $(networksetup -setdnsservers Wi-Fi $dns) && echo "I have correctly set the DNS $dns for the network with SSID $wifiName"
     exit 0
-           }
+}
 function Linux {
     [ "$UID" -eq 0 ] || exec sudo bash "$0" "$@" 
     wifiName=$(iwgetid -r)
     dns=$(cat $dnsConfig | grep "$wifiName" | sed -e 's/\(^.*|\)\(.*\)\(;.*$\)/\2/')
-	sudo -i rm -f "/etc/resolv.conf"
+    sudo -i rm -f "/etc/resolv.conf"
     dns1=$(echo "$dns"  | head -n1 | awk '{print $1;}')
     dns2=$(echo "$dns"  | head -n1 | awk '{print $2;}')
     sudo echo -e  "nameserver $dns1\nnameserver $dns2" >> "/etc/resolv.conf" && echo "I have correctly set the DNS $dns for the network with SSID $wifiName" && exit 0
